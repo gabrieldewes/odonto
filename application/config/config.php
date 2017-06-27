c5557465b515b6320e88da632c4f5e27fccb9a62
Gabriel Dewes <gabrieldewes>
2017-06-27 14:32:37 -0300

    adjusting time ago

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+//header( 'Content-Type: text/html; charset=iso-8859-1' );
+//setlocale(LC_ALL, "pt_BR", "pt_BR.iso-8859-1", "pt_BR.utf-8", "portuguese");
+date_default_timezone_set('America/Sao_Paulo');
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
--- application/helpers/time_ago_helper.php
+++ application/helpers/time_ago_helper.php
@@ -1,17 +1,17 @@
 <?php
   define("TIMEBEFORE_NOW",         'Agora mesmo' );
-  define("TIMEBEFORE_MINUTE",      '{num} minuto atrás' );
+  define("TIMEBEFORE_MINUTE",      'Há cerca de {num} minuto atrás' );
   define("TIMEBEFORE_MINUTES",     'Há cerca de {num} minutos atrás' );
-  define("TIMEBEFORE_HOUR",        '{num} hora atrás' );
+  define("TIMEBEFORE_HOUR",        'Há cerca de {num} hora atrás' );
   define("TIMEBEFORE_HOURS",       'Há cerca de {num} horas atrás' );
   define("TIMEBEFORE_DAYS",        'Há cerca de {num} dias atrás' );
-  define("TIMEBEFORE_WEEK",        '{num} semana atrás' );
+  define("TIMEBEFORE_WEEK",        'Há cerca de {num} semana atrás' );
   define("TIMEBEFORE_WEEKS",       'Há cerca de {num} semanas atrás' );
-  define("TIMEBEFORE_MONTH",       '{num} mês atrás' );
+  define("TIMEBEFORE_MONTH",       'Há cerca de {num} mês atrás' );
   define("TIMEBEFORE_MONTHS",      'Há cerca de {num} meses atrás' );
-  define("TIMEBEFORE_YESTERDAY",   'Ontem' );
-  define("TIMEBEFORE_FORMAT",      '%e %b' );
-  define("TIMEBEFORE_FORMAT_YEAR", '%e %b, %Y' );
+  define("TIMEBEFORE_YESTERDAY",   'Ontem ás {num}' );
+  define("TIMEBEFORE_FORMAT",      '%A, %d de %B às %H:%M' );
+  define("TIMEBEFORE_FORMAT_YEAR", '%A, %d de %B de %Y às %H:%M' );
 
   function time_ago( $time ) {
     $out    = '';
@@ -28,7 +28,7 @@
       return str_replace( '{num}', ( $out = round( $diff / 3600 ) ), $out == 1 ? TIMEBEFORE_HOUR : TIMEBEFORE_HOURS );
 
     elseif( $diff < 3600 * 24 * 2 )
-      return TIMEBEFORE_YESTERDAY;
+      return str_replace( '{num}', date("H:i", $time), TIMEBEFORE_YESTERDAY );
 
     elseif( $diff < 3600 * 24 * 7 )
       return str_replace( '{num}', round( $diff / ( 3600 * 24 ) ), TIMEBEFORE_DAYS );
@@ -40,7 +40,7 @@
       return str_replace( '{num}', ( $out = round( $diff / ( 3600 * 24 * 7 * 4 ) ) ), $out == 1 ? TIMEBEFORE_MONTH : TIMEBEFORE_MONTHS );
 
     else
-      return strftime( date( 'Y', $time ) == date( 'Y' ) ? TIMEBEFORE_FORMAT : TIMEBEFORE_FORMAT_YEAR, $time );
+      return ucfirst(strftime( date( 'Y', $time ) == date( 'Y' ) ? TIMEBEFORE_FORMAT : TIMEBEFORE_FORMAT_YEAR, $time ));
 
   }
 
